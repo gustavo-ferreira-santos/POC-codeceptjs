12feab5d27d351c662615d9890bd4f53d836eba5
Gustavo Ferreira Santos <47151735+gustavo-ferreira-santos@users.noreply.github.com>
2021-07-27 09:26:05 -0300

    Update search_service_test.js

--- tests/cli/search_service_test.js
+++ tests/cli/search_service_test.js
@@ -13,7 +13,7 @@ Scenario('Search Service With Success @search_success', ({I}) => {
     })
 
     I.runOnIOS(() => {
-      console.log('Executando teste no Android')
+      console.log('Executando teste no iOS')
       search_service_page.enterCli('11948101272')
       search_service_page.searchService('pintor')
 
